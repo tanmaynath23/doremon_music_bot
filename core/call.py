c70161bd2c4be976ed9720e1ff624a956757e773
Tanmay <165928657+tanmaynath23@users.noreply.github.com>
2026-09-20 19:00:40 +0530

    Remove error handling for group call exceptions

--- core/call.py
+++ core/call.py
@@ -1,8 +1,3 @@
-import pyrogram.errors
-for _n in ["GroupcallForbidden","GroupCallForbidden","GroupcallInvalid","GroupCallInvalid","GroupcallNotModified","GroupCallNotModified","GroupCallDiscarded"]:
-    if not hasattr(pyrogram.errors, _n):
-        setattr(pyrogram.errors, _n, type(_n, (Exception,), {}))
-
 from pytgcalls import PyTgCalls
 from core.userbot import user
 pytg = PyTgCalls(user)
