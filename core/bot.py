6eac57d1dbff903813919a7fc168fcd16a47b222
Tanmay <165928657+tanmaynath23@users.noreply.github.com>
2026-09-20 20:31:01 +0530

    Refactor bot initialization to use environment variables

--- core/bot.py
+++ core/bot.py
@@ -1,3 +1,6 @@
 from pyrogram import Client
-from config import API_ID, API_HASH, BOT_TOKEN
-app = Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, plugins=dict(root="plugins"))
+import os
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+bot = Client("doremon-bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, plugins=dict(root="plugins"))
