7dfa88d343a43310a11020853dd5ab17444f2111
Tanmay <165928657+tanmaynath23@users.noreply.github.com>
2026-09-20 20:32:27 +0530

    Patch missing group call error types in pyrogram

--- core/call.py
+++ core/call.py
@@ -1,20 +1,11 @@
 import pyrogram.raw.types
 import pyrogram.errors
-
-# ye sab missing cheezo ko auto-create kar dega
-def _patch_missing(name, module):
-    if not hasattr(module, name):
-        setattr(module, name, type(name, (Exception,), {}))
-
-# Raw types patch
 if not hasattr(pyrogram.raw.types, 'InputGroupCallSlug'):
     pyrogram.raw.types.InputGroupCallSlug = type('InputGroupCallSlug', (), {})
-
-# Errors patch - saare Groupcall wale
-for err_name in ['GroupcallInvalid', 'GroupcallForbidden', 'GroupcallNotModified', 'GroupcallAlreadyJoined', 'GroupcallJoinMissing']:
-    _patch_missing(err_name, pyrogram.errors)
+for n in ['GroupcallInvalid','GroupcallForbidden','GroupcallNotModified','GroupcallAlreadyJoined','GroupcallJoinMissing']:
+    if not hasattr(pyrogram.errors, n):
+        setattr(pyrogram.errors, n, type(n, (Exception,), {}))
 
 from pytgcalls import PyTgCalls
 from core.userbot import user
-
 pytg = PyTgCalls(user)
