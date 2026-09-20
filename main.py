7e40bcdcf416465dd828eb2e2c19ee0f50d439b1
Tanmay <165928657+tanmaynath23@users.noreply.github.com>
2026-09-20 12:52:02 +0530

    Add imports and environment variables for bot setup

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import os
+from pyrogram import Client, filters
+from pytgcalls import PyTgCalls, idle
+from pytgcalls.types import MediaStream
+import yt_dlp
 
+API_ID = int(os.getenv("32223763"))
+API_HASH = os.getenv("74845a85442af570c0d0545c9a223799")
+BOT_TOKEN = os.getenv("8972400222:AAF6a5LfKIjBIA0pTV7qUFOoNk5j6ty")
+STRING_SESSION = os.getenv("STRING_SESSION")
