98be28fd42bdf0f3b5d01eefe261bb3e65679dfc
Tanmay <165928657+tanmaynath23@users.noreply.github.com>
2026-09-20 20:31:36 +0530

    Replace bot token with session string for user client

--- core/bot.py
+++ core/bot.py
@@ -2,5 +2,5 @@ from pyrogram import Client
 import os
 API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-bot = Client("doremon-bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, plugins=dict(root="plugins"))
+SESSION = os.environ.get("SESSION_STRING")
+user = Client("doremon-user", api_id=API_ID, api_hash=API_HASH, session_string=SESSION)
