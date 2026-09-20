595d81dfcc2c2bc749c47c0a868bab0de6c8602d
Tanmay <165928657+tanmaynath23@users.noreply.github.com>
2026-09-20 15:28:22 +0530

    Initialize Telegram bot with Pyrogram and PyTgCalls

---
+++ main.py
@@ -0,0 +1,24 @@
+from pyrogram import Client
+from pytgcalls import PyTgCalls, idle
+import os
+
+API_ID = int(os.environ.get("32223763"))
+API_HASH = os.environ.get("74845a85442af570c0d0545c9a223799")
+BOT_TOKEN = os.environ.get("8972400222:AAF6a5LfKIjBIA0pTV7qUFOoNk5j6ty")
+
+app = Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+call = PyTgCalls(app)
+
+@app.on_message()
+async def start(client, message):
+    await message.reply("Bot Live Hai ✅")
+
+async def main():
+    await app.start()
+    await call.start()
+    print("Bot Started ✅")
+    await idle()
+    await app.stop()
+
+import asyncio
+asyncio.get_event_loop().run_until_complete(main())
